45cb5682eef0b1cd373839460a7d644c92aade9a
kearaaa <dekaraa15@gmail.com>
2023-11-13 15:54:41 +0700

    add fitur multiuser

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Officer',
+                'email' => 'officer@gmail.com',
+                'role' => 'officer',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
