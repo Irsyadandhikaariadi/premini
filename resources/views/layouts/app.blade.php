4791483fcfcebf64e8447203c48ae821bad71a43
Irsyadandhikaariadi <113318445+Irsyadandhikaariadi@users.noreply.github.com>
2023-11-16 09:46:49 +0700

    penambahan foto dan pembenahan dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ config('app.name', 'Gym') }}</title>
+  <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.bunny.net">
