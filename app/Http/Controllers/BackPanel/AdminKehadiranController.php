f4ec1144644cf2bd2323b9ffa6fe0a447c488513
kearaaa <dekaraa15@gmail.com>
2023-11-20 09:14:24 +0700

    fix error jadwal user

--- app/Http/Controllers/BackPanel/AdminKehadiranController.php
+++ app/Http/Controllers/BackPanel/AdminKehadiranController.php
@@ -14,6 +14,7 @@ class AdminKehadiranController extends Controller
     public function index()
     {
         //
+        return view('admin.kehadiran.kehadiran');
     }
 
     /**
--- resources/views/jadwal/jadwal.blade.php
+++ resources/views/jadwal/jadwal.blade.php
@@ -6,13 +6,12 @@
         <div class="p-6 text-gray-900 dark:text-gray-100">
           <a href="{{ route('jadwal.create') }}">tambah</a>
           @foreach ($jadwal as $jadwal)
-              
+            <a href="#"
+              class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $jadwal->note }}</h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">{{ $jadwal->waktu }}</p>
+            </a>
           @endforeach
-          <a href="#"
-            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $jadwal->note }}</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">{{ $jadwal->waktu }}</p>
-          </a>
         </div>
       </div>
     </div>
