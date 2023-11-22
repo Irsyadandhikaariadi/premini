53eea33da312ec5b03d6ec377e19a46e43593868
kearaaa <dekaraa15@gmail.com>
2023-11-22 15:35:31 +0700

    merapikan nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,70 +1,24 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-  <!-- Primary Navigation Menu -->
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="flex justify-between h-16">
-      <div class="flex">
-        <!-- Logo -->
-        <div class="shrink-0 flex items-center">
-          <a href="{{ route('dashboard') }}">
-            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-          </a>
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="{{ route('dashboard') }}">
+      <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+    </a>
+    <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <button type="button"
+        class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+        id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+        <span class="sr-only">Open user menu</span>
+        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo">
+      </button>
+      <!-- Dropdown menu -->
+      <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+        id="user-dropdown">
+        <div class="px-4 py-3">
+          <span class="block text-sm text-gray-900 dark:text-white">{{ Auth::user()->name }}</span>
+          <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ Auth::user()->email }}</span>
         </div>
-
-        <!-- Navigation Links -->
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-          @if (Auth::user()->role == 'user')
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-              {{ __('Dashboard') }}
-            </x-nav-link>
-            <x-nav-link :href="route('latihan.user')" :active="request()->routeIs('latihan.user')">
-              {{ __('Latihan') }}
-            </x-nav-link>
-            <x-nav-link :href="route('kehadiran.user')" :active="request()->routeIs('kehadiran.user')">
-              {{ __('Kehadiran') }}
-            </x-nav-link>
-            <x-nav-link :href="route('jadwal.user')" :active="request()->routeIs('jadwal.user')">
-              {{ __('Jadwal') }}
-            </x-nav-link>
-          @endif
-          @if (Auth::user()->role == 'admin')
-            <x-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
-              {{ __('Dashboard') }}
-            </x-nav-link>
-            <x-nav-link :href="route('anggota')" :active="request()->routeIs('anggota')">
-              {{ __('Anggota') }}
-            </x-nav-link>
-            <x-nav-link :href="route('latihan.admin')" :active="request()->routeIs('latihan.admin')">
-              {{ __('Latihan') }}
-            </x-nav-link>
-            <x-nav-link :href="route('womenu.admin')" :active="request()->routeIs('womenu.admin')">
-              {{ __('Womenu') }}
-            </x-nav-link>
-            <x-nav-link :href="route('kehadiran.admin')" :active="request()->routeIs('kehadiran.admin')">
-              {{ __('Kehadiran') }}
-            </x-nav-link>
-          @endif
-        </div>
-      </div>
-
-      <!-- Settings Dropdown -->
-      <div class="hidden sm:flex sm:items-center sm:ms-6">
-        <x-dropdown align="right" width="48">
-          <x-slot name="trigger">
-            <button
-              class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-              <div>{{ Auth::user()->name }}</div>
-
-              <div class="ms-1">
-                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                  <path fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd" />
-                </svg>
-              </div>
-            </button>
-          </x-slot>
-
-          <x-slot name="content">
+        <ul class="py-2" aria-labelledby="user-menu-button">
+          <li>
             <x-dropdown-link :href="route('profile.edit')">
               {{ __('Profile') }}
             </x-dropdown-link>
@@ -75,91 +29,55 @@
 
               <x-dropdown-link :href="route('logout')"
                 onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                  this.closest('form').submit();">
                 {{ __('Log Out') }}
               </x-dropdown-link>
             </form>
-          </x-slot>
-        </x-dropdown>
-        <button id="mode-toggle" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
-          Toggle Mode
-        </button>
+          </li>
+        </ul>
       </div>
-
-      <!-- Hamburger -->
-      <div class="-me-2 flex items-center sm:hidden">
-        <button @click="open = ! open"
-          class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round"
-              stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-              stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
+      <button data-collapse-toggle="navbar-user" type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-user" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
     </div>
-  </div>
-
-  <!-- Responsive Navigation Menu -->
-  <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-    <div class="pt-2 pb-3 space-y-1">
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 gap-x-10" id="navbar-user">
       @if (Auth::user()->role == 'user')
-        <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
           {{ __('Dashboard') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('latihan.user')" :active="request()->routeIs('latihan')">
+        </x-nav-link>
+        <x-nav-link :href="route('latihan.user')" :active="request()->routeIs('latihan.user')">
           {{ __('Latihan') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('kehadiran.user')" :active="request()->routeIs('kehadiran.user')">
+        </x-nav-link>
+        <x-nav-link :href="route('kehadiran.user')" :active="request()->routeIs('kehadiran.user')">
           {{ __('Kehadiran') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('jadwal.user')" :active="request()->routeIs('jadwal.user')">
+        </x-nav-link>
+        <x-nav-link :href="route('jadwal.user')" :active="request()->routeIs('jadwal.user')">
           {{ __('Jadwal') }}
-        </x-responsive-nav-link>
+        </x-nav-link>
       @endif
       @if (Auth::user()->role == 'admin')
-        <x-responsive-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
+        <x-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
           {{ __('Dashboard') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('anggota')" :active="request()->routeIs('anggota')">
+        </x-nav-link>
+        <x-nav-link :href="route('anggota')" :active="request()->routeIs('anggota')">
           {{ __('Anggota') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('latihan.admin')" :active="request()->routeIs('latihan.admin')">
+        </x-nav-link>
+        <x-nav-link :href="route('latihan.admin')" :active="request()->routeIs('latihan.admin')">
           {{ __('Latihan') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('womenu.admin')" :active="request()->routeIs('womenu.admin')">
+        </x-nav-link>
+        <x-nav-link :href="route('womenu.admin')" :active="request()->routeIs('womenu.admin')">
           {{ __('Womenu') }}
-        </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('kehadiran.admin')" :active="request()->routeIs('kehadiran.admin')">
+        </x-nav-link>
+        <x-nav-link :href="route('kehadiran.admin')" :active="request()->routeIs('kehadiran.admin')">
           {{ __('Kehadiran') }}
-        </x-responsive-nav-link>
+        </x-nav-link>
       @endif
-    </div>
-
-    <!-- Responsive Settings Options -->
-    <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-      <div class="px-4">
-        <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-      </div>
-
-      <div class="mt-3 space-y-1">
-        <x-responsive-nav-link :href="route('profile.edit')">
-          {{ __('Profile') }}
-        </x-responsive-nav-link>
-
-        <!-- Authentication -->
-        <form method="POST" action="{{ route('logout') }}">
-          @csrf
-
-          <x-responsive-nav-link :href="route('logout')"
-            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-            {{ __('Log Out') }}
-          </x-responsive-nav-link>
-        </form>
-      </div>
     </div>
   </div>
 </nav>
